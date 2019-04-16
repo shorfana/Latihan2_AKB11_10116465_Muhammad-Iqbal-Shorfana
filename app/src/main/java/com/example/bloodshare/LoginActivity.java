f9632107c9f80845a6212378fc6127d1fbf49a64
iqbal <iqbalshorfana@gmail.com>
2019-04-16 12:42:27 +0700

    finish

--- app/src/main/java/com/example/bloodshare/LoginActivity.java
+++ app/src/main/java/com/example/bloodshare/LoginActivity.java
@@ -5,21 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login = (Button)findViewById(R.id.btn_Login);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent loginn  = new Intent(getApplicationContext(),RegisterActivity.class);
-                startActivity(loginn);
-            }
-        });
+
+
+    }
+
+    public void onClick(View v){
+        Intent intent = null;
+        switch(v.getId()){
+            case R.id.btn_Register:
+                intent = new Intent(this,RegisterActivity.class);
+                break;
+        }
+        if (null!=intent) startActivity(intent);
     }
 }
